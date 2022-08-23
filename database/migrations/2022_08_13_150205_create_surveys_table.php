b9f941bb4c212a65fb6f5e76f292fcf58e4937c4
Death <M.26A20.M@gmail.com>
2022-08-23 12:51:34 +0430

    minor changes

--- database/migrations/2022_08_13_150205_create_surveys_table.php
+++ database/migrations/2022_08_13_150205_create_surveys_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -15,7 +16,7 @@ return new class extends Migration
    {
       Schema::create('surveys', function (Blueprint $table) {
          $table->id();
-         $table->foreignIdFor(\App\Models\User::class, 'user_id');
+         $table->foreignId('user_id')->constrained()->nullable();
          $table->string('title', 1000);
          $table->string('slug', 1000);
          $table->tinyInteger('status');
